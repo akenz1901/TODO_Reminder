067d8fb8679b9ffcda8eba05fb67f0fb7d22cc90
akenz1901 <michael_aka1@icloud.com>
2021-11-05 23:26:14 +0100

    Reminder refactored

--- com/todos/notification_trigger.py
+++ com/todos/notification_trigger.py
@@ -1,58 +1,74 @@
 from win10toast import ToastNotifier
 import time
 
-list_of_to_do = []
-
-predicted_time = 1 * 10
+list_of_to_do = {""}
 
 
-def toDoInterFace():
-    take = str(input("Enter A TODO: "))
-    list_of_to_do.append(take)
-    return take
+def store_name_and_message(name: str, take: str):
+    list_of_to_do.update(name, take)
 
 
-def get_to_do():
-    for i in list_of_to_do:
-        return i
+def getListOfToDo():
+    return list_of_to_do
 
 
-def time_calculator(minutes):
-    return time.sleep(minutes)
+# def get_to_do():
+#     for i in list_of_to_do:
+#         return i
 
 
-def todo_notifier():
-    notification_toaster = ToastNotifier()
-    message = get_to_do()
-    name = "TODO"
-    time_calculator(1800)
-    notification_toaster.show_toast(name, message)
+def time_calculator(seconds):
+    return time.sleep(seconds)
 
+
+# def todo_notifier():
+#     notification_toaster = ToastNotifier()
+#     message = get_to_do()
+#     name = "TODO"
+#     time_calculator(1800)
+#     notification_toaster.show_toast(name, message)
+# todo_notifier()
 
 def store_todo():
-    toDoInterFace()
-    store_reminder_time()
     print("Is there more TODO to add ?\n.\n.\n.\nEnter Yes if you have more todo and No if there is no more TODO")
-    decision = str(input("Or enter Y/N"))
+    decision = str(input("Or enter Y/N: "))
     return decision
 
 
 def store_reminder_time():
-    thread_for_each_todo = []
-    minutes = int(input("Enter Notification Time: "))
-    thread_for_each_todo.append(minutes)
+    minutes = int(input("How many minutes time do you intend to get notification: "))
+    tem_time = minutes * 60
+    return tem_time
 
 
 def new_to_app():
     notification_toaster = ToastNotifier()
+    thread_for_each_todo = []
     print("Hey Welcome")
+    name = str(input("Enter to name: "))
+    take = str(input("Enter A TODO: "))
     while True:
+        store_name_and_message(name, take)
+        # thread_for_each_todo.append(store_reminder_time())
         store_todo()
         if store_todo().capitalize().startswith("Y"):
             print("You can store as much as you desire")
-            pass
+            continue
         elif store_todo().capitalize().startswith("N"):
-            return False
+            break
+    print("Saving........")
+    time_calculator(seconds=10)
+    print("Saved!")
+    for t in thread_for_each_todo:
+        time_calculator(seconds=t)
+        i = 0
+        ii = 1
+        for n, m in getListOfToDo():
+            notification_toaster.show_toast(n, m)
+            i += 1
+            if i is ii:
+                ii += 1
+                break
 
 
-todo_notifier()
+new_to_app()
