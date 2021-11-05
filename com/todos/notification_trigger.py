d19deadb6d3ed2640e86849ebde284db8656bad4
akenz1901 <michael_aka1@icloud.com>
2021-11-05 22:18:22 +0100

    To do reminder app

--- com/todos/notification_trigger.py
+++ com/todos/notification_trigger.py
@@ -8,11 +8,11 @@ predicted_time = 1 * 10
 
 def toDoInterFace():
     take = str(input("Enter A TODO: "))
+    list_of_to_do.append(take)
     return take
 
 
 def get_to_do():
-    list_of_to_do.append(toDoInterFace())
     for i in list_of_to_do:
         return i
 
@@ -22,11 +22,37 @@ def time_calculator(minutes):
 
 
 def todo_notifier():
+    notification_toaster = ToastNotifier()
     message = get_to_do()
     name = "TODO"
-    notification_toaster = ToastNotifier()
-    time_calculator(60)
+    time_calculator(1800)
     notification_toaster.show_toast(name, message)
+
+
+def store_todo():
+    toDoInterFace()
+    store_reminder_time()
+    print("Is there more TODO to add ?\n.\n.\n.\nEnter Yes if you have more todo and No if there is no more TODO")
+    decision = str(input("Or enter Y/N"))
+    return decision
+
+
+def store_reminder_time():
+    thread_for_each_todo = []
+    minutes = int(input("Enter Notification Time: "))
+    thread_for_each_todo.append(minutes)
+
+
+def new_to_app():
+    notification_toaster = ToastNotifier()
+    print("Hey Welcome")
+    while True:
+        store_todo()
+        if store_todo().capitalize().startswith("Y"):
+            print("You can store as much as you desire")
+            pass
+        elif store_todo().capitalize().startswith("N"):
+            return False
 
 
 todo_notifier()
